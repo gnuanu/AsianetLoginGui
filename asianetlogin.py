7fad6d6aa08d45c2608853e80ef99c144ec3bc3a
Anoop Panavalappil <gnuanu@gmail.com>
2010-06-11 23:45:34 +0530

    Trimmed down the code, Changed Log Class to a single function, changed Main class to a thread

--- asianetlogin.py
+++ asianetlogin.py
@@ -14,18 +14,23 @@ class AsianetLogin(QtGui.QDialog, Ui_AsianetLogin):
 	def __init__(self, parent = None):
 		super(AsianetLogin, self).__init__(parent)
 		self.setupUi(self)
+		self.running = False
+		
 		
 		if os.path.exists("asianetconf"):
 			file = open("asianetconf", 'r')
 			text = file.readline()
-			data = text.split("=") 
+			data = text.split("=")
 			self.edUsername.setText(data[0])
 			self.edPassword.setText(data[1])
 			file.close()
 		
+		self.login = AsianetLoginMain(self)
+		self.connect(self.login, QtCore.SIGNAL("diplayMessage(QString)"), self.displayLog)
+		
 		self.connect(self.bnClose, QtCore.SIGNAL("clicked()"), self.close)
 		self.connect(self.bnConnect, QtCore.SIGNAL("clicked()"), self.onConnect)
-
+	
 	def onConnect(self):
 		if self.edUsername.text() == "" or self.edPassword.text() == "":
 			QtGui.QMessageBox.warning(self, "Error - AsianetLogin", "Username and Password are mandatory fields")
@@ -35,19 +40,23 @@ class AsianetLogin(QtGui.QDialog, Ui_AsianetLogin):
 			file = open("asianetconf", 'w')
 			file.write(self.edUsername.text() + "=" + self.edPassword.text())
 			file.close()
-		
-		self.login = AsianetLoginMain(self.edUsername.text(), self.edPassword.text(), self)
-		self.connect(self.login.log, QtCore.SIGNAL("displayMessage(QString)"), self.displayLog)	
-		self.login.connectAsianet()
 			
+		if not self.running:			
+			self.login.initialize(self.edUsername.text(), self.edPassword.text())
+			self.login.start()
+			self.running = True
+		else:
+			if self.login.isFinished():
+				self.running = False
+				self.onConnect()
+	
 	def displayLog(self, log):
 		display_log = self.teLog.toPlainText()
-		display_log += log
+		display_log += log + "\n"
 		self.teLog.setText(display_log)
-		self.teLog.update()
 		scrollbar = self.teLog.verticalScrollBar()
 		scrollbar.setValue(scrollbar.maximum())
-		
+
 if __name__ == "__main__":
 	app = QtGui.QApplication(sys.argv)
 	
--- asianetloginmain.py
+++ asianetloginmain.py
@@ -1,124 +1,76 @@
 '''
 Created on Jun 7, 2010
 
-@author: Anoop Panavalappil
+@author: anoop.panavalappil
 '''
 
 from PyQt4 import QtCore
 import httplib, urllib
 import time
-import os.path
 
-
-class Log(QtCore.QThread):
-	def __init__(self):
-		super(Log, self).__init__()
-		logfile = "asianet_log"
-		self.log = open(logfile, 'a')
-		
-	def initialize(self, message):
-		print message
-		self.logmsg = time.ctime() + " - " + message + '\n'
-		self.log.write(self.logmsg)
-		
-	def run(self):
-		self.emit(QtCore.SIGNAL("displayMessage(QString)"), self.logmsg)
-		
-	def __del__(self):
-		self.log.close()
-		
-class AsianetLoginMain:
-	def __init__(self, username, password, parent = None):
-		self.username = username
-		self.password = password
+class AsianetLoginMain(QtCore.QThread):
+	def __init__(self, parent = None):
+		super(AsianetLoginMain, self).__init__()
 		self.parent = parent
 		
 		self.accept = "Login"
 		self.url_file = "asianet_url"
+		self.log = open("asianet_log", 'a')
 		
-		self.log = Log()
+	def initialize(self , username, password):
+		self.username = username
+		self.password = password
 		
-	def connectAsianet(self):
-		self.log.initialize("Starting Asianet auto login...")
-		self.log.start()
-		self.log.wait()
-			
+	def run(self):
+		self.writeLog("Starting Asianet auto login...")
+		
 		check_url = "www.google.com"
 		check_res = "/intl/en/privacy.html"
 		
 		conn = httplib.HTTPConnection(check_url)
 		
 		try:
-			self.log.initialize("Checking current connection status")
-			self.log.start()
-			self.log.wait()
+			self.writeLog("Checking current connection status")
 			
-			self.log.initialize("Connecting to " + check_url)
-			self.log.start()
-			self.log.wait()
-			
+			self.writeLog("Connecting to " + check_url)
 			conn.request("GET", check_res)
 			
 			res = conn.getresponse()
 			conn.close()
 			
-			self.log.initialize("Response = {0}".format(httplib.responses[res.status]))
-			self.log.start()
-			self.log.wait()
+			self.writeLog("Response = {0}".format(httplib.responses[res.status]))
 			
 			if res.status == 200 or res.status == 202:
-				self.log.initialize("An active connection is already present")
-				self.log.start()
-				self.log.wait()
+				self.writeLog("An active connection is already present")
 				
 			if res.status == 302:
-				self.log.initialize("Couldn't connect to google")
-				self.log.start()
-				self.log.wait()
-				
+				self.writeLog("Couldn't connect to " + check_url)
 				login_url = res.getheader('location').split("?")[0]
 				self.loginToInternet(login_url)
 		except:
-			self.log.initialize("Network error. Check your connection")
-			self.log.start()
-			self.log.wait()
-			
+			self.writeLog("Network error. Check your connection")
+	
 	def loginToInternet(self, login_url):
-		self.log.initialize("Logging in to Asianet..")
-		self.log.start()
-		self.log.wait()
-		
-		self.saveLoginUrl(login_url)
+		self.writeLog("Logging in to Asianet..")
 		params = urllib.urlencode({'auth_user': self.username, 'auth_pass': self.password, 'accept': self.accept})
 		response = urllib.urlopen(login_url, params).info()
-		self.log.initialize("Response = {0}".format(response))
-		self.log.start()
-		self.log.wait()
-		
+		self.writeLog("Response = {0}".format(response.status))
 		if response.status == 200 or response.status == 202 or response.status == "":
-			self.log.initialize("Login Successful")
-			self.log.start()
-			self.log.wait()
+			self.writeLog("Login Successful")
 		else:
-			self.log.initialize("Login Failed. Try again later..")
-			self.log.start()
-			self.log.wait()
-			
-	def getLoginUrl(self):
-		url = ""
-		if os.path.exists(self.url_file):
-			urlfile = open(self.url_file, 'r')
-			url = urlfile.readline()
-			urlfile.close()
-		return url
+			self.writeLog("Login Failed. Try again later..")
 	
-	def saveLoginUrl(self, url):
-		urlfile = open(self.url_file, 'w')
-		urlfile.write(url)
-		urlfile.close()
-	
+	def writeLog(self, message):
+		print message
+		self.message = message
+		log = time.ctime() + " - " + message + '\n'
+		self.log.write(log)
+		self.emit(QtCore.SIGNAL("diplayMessage(QString)"), self.message)
+
 if __name__ == "__main__":
 	username = "your username"
 	password = "your password"
-	login = AsianetLoginMain(username, password)
-	login.connectAsianet()
+	login = AsianetLoginMain()
+	login.initialize(username, password)
+	login.start()
+	login.wait()
