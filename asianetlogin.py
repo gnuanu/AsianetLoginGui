3f1f6448687e8e2a317a0d187e2b8871beee69e3
Anoop Panavalappil <gnuanu@gmail.com>
2010-06-14 00:14:12 +0530

    Added file headers and license statement

--- asianetlogin.py
+++ asianetlogin.py
@@ -1,10 +1,35 @@
-'''
-Created on Jun 7, 2010
+#========================================================================#
+# File Name		: asianetlogin.py
+# Date			: Jun 7, 2010
+# Author		: Anoop Panavalappil
+# Description	: Asianet Login GUI Class Implementation
+#========================================================================#
 
-@author: Anoop Panavalappil
-'''
-from ui_asianetlogin import Ui_AsianetLogin
-from asianetloginmain import AsianetLoginMain
+"""
+Copyright 2010 Anoop Panavalappil <gnuanu@gmail.com@>
+http://www.librecode.org
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Library General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+If you find any bugs or have any suggestions email: gnuanu@gmail.com
+URL: http://www.librecode.org
+"""
+
+from ui_asianetlogin import Ui_AsianetLogin		# The generated UI class
+from asianetloginmain import AsianetLoginMain	# The main class
+
 from PyQt4 import QtCore, QtGui
 
 import os.path
--- asianetloginmain.py
+++ asianetloginmain.py
@@ -1,10 +1,34 @@
-'''
-Created on Jun 7, 2010
+#========================================================================#
+# File Name		: asianetloginmain.py
+# Date			: Jun 7, 2010
+# Author		: Anoop Panavalappil
+# Description	: Asianet Login Main Class Implementation (command line)
+#========================================================================#
 
-@author: anoop.panavalappil
-'''
+"""
+Copyright 2010 Anoop Panavalappil <gnuanu@gmail.com@>
+http://www.librecode.org
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Library General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+If you find any bugs or have any suggestions email: gnuanu@gmail.com
+URL: http://www.librecode.org
+"""
 
 from PyQt4 import QtCore
+
 import httplib, urllib
 import time
 
@@ -70,7 +94,9 @@ class AsianetLoginMain(QtCore.QThread):
 if __name__ == "__main__":
 	username = "your username"
 	password = "your password"
+	
 	login = AsianetLoginMain()
 	login.initialize(username, password)
+	
 	login.start()
 	login.wait()
